88763c9ac9c61f74e655d80544e0ea6b91da2cd5
11gather11 <11gather11@gmail.com>
2025-11-22 17:58:47 +0900

    chore: update ESLint configuration and dependencies
    - Added eslint-plugin-astro to ESLint configuration for Astro support.
    - Updated eslint.config.mjs to extend recommended configurations from eslint-plugin-astro and @typescript-eslint.
    - Added @typescript-eslint/parser and eslint-plugin-jsx-a11y to package.json dependencies.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,17 @@ import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import { defineConfig } from 'eslint/config'
+import eslintPluginAstro from 'eslint-plugin-astro'
 
 export default defineConfig([
+  js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...tseslint.configs.recommended,
 	{
 		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-		plugins: { js },
 		extends: ['js/recommended'],
 		languageOptions: { globals: globals.browser },
 	},
-	tseslint.configs.recommended,
 	{
 		ignores: ['dist', 'node_modules', '.github', 'types.generated.d.ts', '.astro'],
 	},
