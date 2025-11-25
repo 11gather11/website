69c9105b84879efd2d385c62ca6323de7655ef57
11gather11 <11gather11@gmail.com>
2025-11-25 10:23:47 +0900

    chore: ESLintの設定と依存関係を更新
    - ESLintの設定をリファクタリングし、TypeScriptとAstroのサポートのために追加のプラグインとパーサーを含めました。
    - simple import sortingとJSXアクセシビリティのためのルールを追加しました。
    - knipの設定を更新し、'wrangler'依存関係を無視するようにしました。
    - lefthookのpre-commitコマンドを強化し、ステージングされたファイルに対してESLintを実行するようにしました。
    - @typescript-eslint/parser、astro-eslint-parser、eslint-config-prettier、eslint-plugin-import、eslint-plugin-simple-import-sort、typescript-eslintを含む、いくつかの依存関係のバージョンを上げました。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,69 @@
-import js from '@eslint/js'
+import pluginJs from '@eslint/js'
+import typescriptEslintParser from '@typescript-eslint/parser'
+import astroParser from 'astro-eslint-parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginAstro from 'eslint-plugin-astro'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tailwindcss from 'eslint-plugin-tailwindcss'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
-import eslintPluginAstro from 'eslint-plugin-astro'
 
-export default defineConfig([
-  js.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  ...tseslint.configs.recommended,
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+	pluginJs.configs.recommended,
+
+	importPlugin.flatConfigs.recommended,
+
+	...tseslint.configs.recommended,
+
+	...tailwindcss.configs['flat/recommended'],
+
 	{
-		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-		extends: ['js/recommended'],
+		plugins: {
+			'jsx-a11y': jsxA11yPlugin,
+		},
+	},
+
+	...eslintPluginAstro.configs['jsx-a11y-recommended'],
+
+	{
 		languageOptions: { globals: globals.browser },
 	},
+
+	{
+		settings: {
+			'import/resolver': {
+				typescript: {},
+			},
+		},
+	},
+
+	{
+		files: ['*.astro'],
+		languageOptions: {
+			parser: astroParser,
+			parserOptions: {
+				parser: typescriptEslintParser,
+				extraFileExtensions: ['.astro'],
+			},
+		},
+	},
+
+	{
+		plugins: {
+			'simple-import-sort': simpleImportSort,
+		},
+		rules: {
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+		},
+	},
+
+	eslintConfigPrettier,
+
 	{
 		ignores: ['dist', 'node_modules', '.github', 'types.generated.d.ts', '.astro'],
 	},
-])
+]
