0aea98bdb906dac73bbc47eab17b0a08459fba85
11gather11 <11gather11@gmail.com>
2025-12-04 10:12:52 +0900

    refactor: siteConfigからSITEへの移行と不要な依存関係の削除

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,47 @@
+import avatar from './assets/avatar.png'
 import banner from './assets/banners/banner.png'
+
+const NAVIGATORS = [
+	{
+		name: 'Home',
+		href: '/',
+	},
+	{
+		name: 'Archive',
+		href: '/archive',
+	},
+	{
+		name: 'About',
+		href: '/about',
+	},
+] as const
+
+const SOCIAL_LINKS = [
+	{
+		icon: 'tabler:brand-x',
+		link: 'https://x.com/vvvmeovvv',
+	},
+	{
+		icon: 'tabler:brand-youtube',
+		link: 'https://youtube.com/@vvvmeovvv',
+	},
+	{
+		icon: 'tabler:brand-twitch',
+		link: 'https://twitch.tv/vvvmeovvv',
+	},
+	{
+		icon: 'tabler:brand-discord',
+		link: 'https://discord.com/invite/jJkYtgu4HU',
+	},
+	{
+		icon: 'tabler:brand-instagram',
+		link: 'https://www.instagram.com/vvvmeovvv',
+	},
+	{
+		icon: 'tabler:brand-tiktok',
+		link: 'https://www.tiktok.com/@vvvmeovvv',
+	},
+] as const
 
 export const SITE = {
 	name: 'My Awesome Website',
@@ -6,7 +49,13 @@ export const SITE = {
 	site: 'https://11gather11.com',
 	base: '/',
 	trailingSlash: false,
+	avatar: avatar,
 	pageSize: 8,
+	maxCategoryCount: 6,
+	maxTagCount: 12,
+	navigators: NAVIGATORS,
+	socialLinks: SOCIAL_LINKS,
+	sign: 'a 11gather11 Works project',
 } as const
 
 export const METADATA = {
